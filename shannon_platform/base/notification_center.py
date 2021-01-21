3e2507dce635ddf128ae3cd217861096b8d8e724
Kiarash Kiani <hi@kiarash.info>
2021-01-21 11:22:15 +0330

    add notification center module for observer pattern design

---
+++ shannon_platform/base/notification_center.py
@@ -0,0 +1,23 @@
+from typing import Callable, Dict, List
+from shannon_platform.base.singleton import Singleton
+
+
+class NotificationCenter(metaclass=Singleton):
+    def __init__(self) -> None:
+        super().__init__()
+
+        self.__observers: Dict[str, List[Callable]] = {}
+
+    def add_observer(self, name: str, callback: Callable) -> None:
+        if name in self.__observers.keys():
+            self.__observers[name].append(callback)
+        else:
+            self.__observers[name] = [callback]
+
+    def remove_observer(self, callback: Callable) -> None:
+        for name in self.__observers.keys():
+            self.__observers[name].remove(callback)
+
+    def post(self, name: str, user_info: Dict[str, str]) -> None:
+        for callback in self.__observers[name]:
+            callable(user_info)
