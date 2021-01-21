600eb42f7258c986a2f64008f92658eb16ed0273
Kiarash Kiani <hi@kiarash.info>
2021-01-21 12:13:52 +0330

    update serial protocol to support notification center pattern

--- shannon_platform/network/serial.py
+++ shannon_platform/network/serial.py
@@ -1,35 +1,36 @@
-from os import name
-from shannon_platform.base.notification_center import NotificationCenter
-import threading
-from typing import Callable
 from serial import Serial
-from serial.serialutil import PortNotOpenError
 from threading import Thread
-from time import sleep
 
+from shannon_platform.base.notification_center import NotificationCenter
 from shannon_platform.base.singleton import Singleton
 
 
-class SerialService():
-    __adaptor = None
-
-    @staticmethod
-    def adaptor() -> Serial:
-        if SerialService.__adaptor != None:
-            return SerialService.__adaptor
-        else:
-            SerialService.__adaptor = Serial(port='/dev/cu.usbmodem14301', baudrate=9600)
-            sleep(2)
-            return SerialService.__adaptor
+class SerialNetwork(metaclass=Singleton):
+    __adaptor = Serial(port='/dev/cu.usbmodem14301', baudrate=9600)
 
     def __init__(self) -> None:
         super().__init__()
 
-    def request(self, message: str) -> str:
-        self.adaptor().flush()
-        self.adaptor().write(message.encode('ascii'))
-        self.adaptor().write("message\n".encode('ascii'))
-        responce = self.adaptor().readline().decode('ascii')
+        NotificationCenter().add_observer(name="com.shannon.device-send", callback=self.write)
 
-        return responce
+        serial_read_thread = Thread(target=self.read)
+        serial_read_thread.start()
+        
+    def read(self):
+        while True:
+            data = self.__adaptor.read(2)
+            if data:
+                id = data[0]
+                value = data[1]
 
+                NotificationCenter().post(
+                    name="com.shannon.device-receive", 
+                    user_info={
+                        'id': id,
+                        'value': value
+                    }
+                )
+
+    def write(self, user_info):
+        command = bytes([user_info['id'], user_info['value']])
+        self.__adaptor.write(command)
