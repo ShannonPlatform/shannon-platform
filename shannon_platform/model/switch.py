8c94a0676a631406cd3e494e0893af813d28f3a2
Kiarash Kiani <hi@kiarash.info>
2021-01-23 18:14:23 +0330

    update switch model to support notification center pattern

--- shannon_platform/model/switch.py
+++ shannon_platform/model/switch.py
@@ -1,12 +1,34 @@
 from __future__ import annotations
 from pydantic import BaseModel, parse_file_as
-from typing import List
+from typing import Any, Dict, List, Optional
+
+from shannon_platform.base.notification_center import NotificationCenter
 
 
 class Switch(BaseModel):
     id: str
-    name: str
-    state: bool
+    name: Optional[str]
 
-    def all() -> List[Switch]:
-        return parse_file_as(List[Switch], '/etc/switches.json')
+    @property
+    def state(self):
+        return False
+
+    @state.setter
+    def state(self, is_on: bool) -> None:
+        if is_on:
+            NotificationCenter().post(
+                name="com.shannon.device-send",
+                user_info={
+                    'id': self.id,
+                    'value': 0x01
+                }
+            )
+        else:
+            NotificationCenter().post(
+                name="com.shannon.device-send",
+                user_info={
+                    'id': self.id,
+                    'value': 0x02
+                }
+            )
+
