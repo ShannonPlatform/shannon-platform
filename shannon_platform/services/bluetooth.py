65bf05b64c4cd01dce6bb5708838320d32a84b1a
Kiarash Kiani <hi@kiarash.info>
2021-01-28 14:20:19 +0330

    fix bluetooth service crashes by adding reconnecting ability

--- shannon_platform/services/bluetooth.py
+++ shannon_platform/services/bluetooth.py
@@ -8,26 +8,40 @@ from shannon_platform.base.metaclasses import Singleton
 
 class BluetoothService:
     def __init__(self, address: str) -> None:
-        self.peripheral = btle.Peripheral()
-        self.peripheral.connect(address, btle.ADDR_TYPE_PUBLIC)
-        self.peripheral.setDelegate(BluetoothNotificationHandler())
-        self.service = self.p.getServiceByUUID("0000ffe0-0000-1000-8000-00805f9b34fb")
-        self.characteristic = self.svc.getCharacteristics()[0]
+        self.address = address
+        self.delegate = BluetoothNotificationHandler()
+
+        self.connect()
 
         NotificationCenter().add_observer(name=NotificationDefaultNames.DATA_SEND, callback=self.write)
 
         read_thread = Thread(target=self.read)
         read_thread.start()
 
+    def connect(self) -> None:
+        self.peripheral = btle.Peripheral()
+        self.peripheral.connect(self.address, btle.ADDR_TYPE_PUBLIC)
+        self.peripheral.setDelegate(self.delegate)
+        self.service = self.peripheral.getServiceByUUID("0000ffe0-0000-1000-8000-00805f9b34fb")
+        self.characteristic = self.service.getCharacteristics()[0]
+
 
     def read(self) -> None:
         while True:
-            if self.p.waitForNotifications(1.0):
+            try:
+                if self.peripheral.waitForNotifications(4.0):
+                    continue
+            except Exception as error:
+                print(error)
+                self.peripheral.disconnect()                
                 continue
 
     def write(self, user_info: Dict[str, Any]) -> None:
-        command = bytes([user_info['id'], user_info['value']])
-        self.characteristic.write(command)
+        try:
+            command = bytes([user_info['id'], user_info['value']])
+            self.characteristic.write(command)
+        except btle.BTLEDisconnectError:
+            self.connect()
     
 
     def request_devices(self) -> None:
@@ -39,23 +53,26 @@ class BluetoothService:
 
 class BluetoothNotificationHandler(btle.DefaultDelegate):
     def handleNotification(self, cHandle, data):
-        if len(data) < 2:
-            return
+        buffer = bytearray(data)
+        
+        for i in range(len(buffer)//2):
+            id = buffer[i*2]
+            value = buffer[i*2+1]
 
-        # device register
-        if data[0] == 0x00:
-            NotificationCenter().post(
-                name=NotificationDefaultNames.REGISTER,
-                user_info={
-                    'id': data[1]
-                }
-            )
-        # data receive
-        else:
-            NotificationCenter().post(
-                name=NotificationDefaultNames.DATA_RECEIVE,
-                user_info={
-                    'id': data[0],
-                    'value': data[1]
-                }
-            )
+            # device register
+            if id == 0x00:
+                NotificationCenter().post(
+                    name=NotificationDefaultNames.REGISTER,
+                    user_info={
+                        'id': value
+                    }
+                )
+            # data receive
+            else:
+                NotificationCenter().post(
+                    name=NotificationDefaultNames.DATA_RECEIVE,
+                    user_info={
+                        'id': id,
+                        'value': value
+                    }
+                )
